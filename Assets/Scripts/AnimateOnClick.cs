0724b7cc860ff52915644f91267a1111936c5a8b
Paul Buser <buser.paul@gmail.com>
2019-12-28 22:47:21 -0500

    Initial commit.

---
+++ Assets/Scripts/AnimateOnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateOnClick : MonoBehaviour
+{
+   bool selected = false;
+   Animator animator; 
+
+   void Start()
+   {
+       animator = GetComponent<Animator>();
+   }
+   void OnMouseDown()
+   {
+       selected = !selected;
+       this.animator.SetBool("isSelected", selected);
+   }
+}
